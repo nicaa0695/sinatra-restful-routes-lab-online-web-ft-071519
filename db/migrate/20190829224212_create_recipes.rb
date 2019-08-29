d4c3e9e8ebb6708ab68224dac942fcb30d61b5b8
Veronica Lopez <nicaa0695@gmail.com>
2019-08-29 22:42:32 +0000

    Automatically backed up by Learn

--- db/migrate/20190829224212_create_recipes.rb
+++ db/migrate/20190829224212_create_recipes.rb
@@ -1,5 +1,10 @@
 class CreateRecipes < ActiveRecord::Migration
   def change
-    
+    def change
+    create_table :recipes do |t|
+      t.string :name
+      t.string :ingredients
+      t.integer :cook_time
+    end
   end
 end
